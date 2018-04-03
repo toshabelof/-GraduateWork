56af6961fdd1b317a6d5f287308c9c7936f237e8
toshabelof <shadow35rus@gmail.com>
2018-04-03 22:38:43 +0300

    Update 0304
    Программа:
    *Интерфейс v0.1
    *Иконки
    Документация:
    *ТЗ
    *Сравнительный анализ

---
+++ Интерфейс v0.1/WpfApplication1/Profile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace WpfApplication1
+{
+    class Profile
+    {
+
+        //head
+        public string Name;
+        public string Position;
+        public string Birth;
+
+        //contacts
+        public string workPhone;
+        public string mobilePhone;
+        public string homePhone;
+        public string skypeID;
+        public string vkID;
+
+        public void Add(string name, string pos, string birth, string wPhone, string mPhone, string hPhone, string sID, string vID)
+        {
+            Name = name;
+            Position = pos;
+            Birth = birth;
+            workPhone = wPhone;
+            mobilePhone = mPhone;
+            homePhone = hPhone;
+            skypeID = sID;
+            vkID = vID;
+        }
+    }
+}
