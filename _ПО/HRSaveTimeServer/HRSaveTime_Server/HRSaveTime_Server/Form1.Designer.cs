cafb7a7e5f9664403e7b362149c07390f07e597a
toshabelof <shadow35rus@gmail.com>
2018-06-18 14:06:34 +0300

    Pred v1

--- _ПО/HRSaveTimeServer/HRSaveTime_Server/HRSaveTime_Server/Form1.Designer.cs
+++ _ПО/HRSaveTimeServer/HRSaveTime_Server/HRSaveTime_Server/Form1.Designer.cs
@@ -609,7 +609,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(652, 400);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "СКУД";
+            this.tabPage2.Text = "Помещения";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // EditRooms_btn
